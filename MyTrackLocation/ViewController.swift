ec7fd240b61e939d18e458eea1eb8f61ca1f95f4
Aimmzz <rohimkurniawan456@gmail.com>
2024-01-09 20:17:27 +0700

    Initial Commit

---
+++ MyTrackLocation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTrackLocation
+//
+//  Created by rohim kurniawan on 09/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
